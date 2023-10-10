5197e2e614b9cfda2dcacba71c2556fd730644f6
aleh <oleg.pilipenko26@gmail.com>
2023-10-10 16:12:11 +0300

    Add user data script for initializing an EC2 instance

---
+++ userdata.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install Java Development Kit (JDK)
+sudo amazon-linux-extras install java-openjdk11
+
+# Install Git
+sudo yum install -y git
+
+# Install Docker
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install Nmap
+sudo yum install -y nmap
+
+# Install Jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins
+
+# Start and enable Jenkins service
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# # Print initial Jenkins admin password
+# echo "Jenkins initial admin password:"
+# sudo cat /var/lib/jenkins/secrets/initialAdminPassword
