8bfabee10710c7c08042988d9a5d278a3824c227
aleh <oleg.pilipenko26@gmail.com>
2023-10-11 15:29:43 +0300

    add install python3 and boto3

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,15 @@
 # Update the system
 sudo yum update -y
 
+#Install ansible
+sudo amazon-linux-extras install ansible2 -y
+
+#Install python3
+sudo yum install python3 -y
+
+#Install boto3
+pip3 install boto3
+
 # Install Java Development Kit (JDK)
 sudo amazon-linux-extras install java-openjdk11
 
@@ -17,9 +26,6 @@ sudo usermod -a -G docker ec2-user
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Install Nmap
-sudo yum install -y nmap
 
 # Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
