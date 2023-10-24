0aade08c9fed14daee8c7b50d858975cbd38d44a
aleh <oleg.pilipenko26@gmail.com>
2023-10-24 22:25:07 +0300

    Add one public subnets

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs = ["eu-central-1a", "eu-central-1b", "eu-central-1c"] 
 
   private_subnets = ["10.0.1.0/24"]
-  public_subnets = ["10.0.4.0/24"]
+  public_subnets = ["10.0.2.0/24","10.0.3.0/24"]
 
  
 }
